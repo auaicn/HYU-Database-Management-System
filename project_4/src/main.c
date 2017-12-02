45d803c94f6c9110154bb3d8111515d4f02d2c59
auaicn <crorowslowpp@gmail.com>
2017-12-02 15:38:41 +0900

    Update main.c
    Former-commit-id: 3015102eb0c408b6068ddb78d9f7ef5e4628a713

--- project_4/src/main.c
+++ project_4/src/main.c
@@ -1,82 +1,85 @@
 #include "bpt.h"
 
-int main(void) {
+int main(int argc, const char* argv[]) {
 
-    int table_id;
-    int table2_id;
-    int size;
-    int64_t input;
-    char instruction;
-    char buf[120];
-    char path[120];
+	int table_id;
+	int table2_id;
+	int size;
+	int64_t input;
+	char instruction;
+	char buf[120];
+	char path[120];
 
- //   print_file();
-//    init_db(20);
-//    open_table("test.db");
-//    printf("> ");
-    while(scanf("%c", &instruction) != EOF){
-        switch(instruction){
-            case 'c':
-                scanf("%d", &table_id);
-                close_table(table_id);
-//                print_buffer();
-//                printf("close table %d!\n", table_id);
-                break;
-            case 'd':
-                scanf("%d %ld", &table_id, &input);
-                delete(table_id, input);
-                break;
-            case 'i':
-                scanf("%d %ld %s", &table_id, &input, buf);
-                insert(table_id, input, buf);
-//                printf("after insert %ld\n", input);
-//                print_file(1);
-                break;
-            case 'f':
-                scanf("%d %ld", &table_id, &input);
-                char * ftest;
-                if((ftest = find(table_id, input)) != NULL){
-                    printf("Key: %ld, Value: %s\n", input, ftest);
-                    fflush(stdout);
-                    free(ftest);
-                }
-                else{
-                    printf("Not Exists\n");
-                    fflush(stdout);
-                }
-                break;
-            case 'j':
-        		scanf("%d %d ",&table_id,&table2_id);
-        		scanf("%s",path);
-        		join(table_id,table2_id,path);
-        		break;
-            case 'l':
-                scanf("%d", &table_id);
-                //print_file(table_id);
-                break;
-            case 'n':
-                scanf("%d", &size);
-                init_db(size);
-//                printf("Buffer setted!\n");
-                break;
-            case 'o':
-                scanf("%s", path);
-                table_id = open_table(path);
-//                printf("%s table ID is: %d\n", path, table_id);
-                break;
-//            case 'p':
-//                print_tree();
-//                break;
-            case 'q':
-                shutdown_db();
-                while(getchar() != (int64_t)'\n');
-                return 0;
-                break;
-        }
-        while(getchar() != (int)'\n');
-//                printf("> ");
-    }
-    printf("\n");
+	// no special function in init()
+	// but for later implementation
+	init();
 
-    return 0;
+	init_db(atoi(argv[1]));
+	usage();
+	printf("%d is set as buffer_pool max size.\n",atoi(argv[1]));
+	printf("> ");
+	while (scanf("%c", &instruction) != EOF) {
+		switch (instruction) {
+		case 'c':
+			scanf("%d", &table_id);
+			close_table(table_id);
+			show_me_buffer();
+			break;
+		case 'd':
+			scanf("%d %ld", &table_id, &input);
+			delete(table_id, input);
+			break;
+		case 'i':
+			scanf("%d %ld %s", &table_id, &input, buf);
+			if (insert(table_id, input, buf) == -1)
+				printf("following key is not existing in leaf page\n");
+			break;
+		case 'f':
+			scanf("%d %ld", &table_id, &input);
+			char * ftest;
+			if ((ftest = find(table_id, input)) != NULL) {
+				printf("Key: %ld, Value: %s\n", input, ftest);
+				fflush(stdout);
+				free(ftest);
+			}
+			else {
+				printf("Not Exists\n");
+				fflush(stdout);
+			}
+			break;
+		case 'u':
+			usage();
+			break;
+		case 'j':
+			scanf("%d %d ", &table_id, &table2_id);
+			scanf("%s", path);
+			join(table_id, table2_id, path);
+			break;
+		case 'n':
+			scanf("%d", &size);
+			init_db(size);
+			printf("Buffer setted!\n");
+			break;
+		case 'o':
+			scanf("%s", path);
+			table_id = open_table(path);
+			printf("%s table ID is: %d\n", path, table_id);
+			break;
+		case 'p':
+			scanf("%d", table_id);
+			display(table_id);
+			break;
+		case 'q':
+			shutdown_db();
+			while (getchar() != (int64_t)'\n');
+			printf("bye\n");
+			return 0;
+			break;
+		}
+		while (getchar() != (int)'\n');
+		//                printf("> ");
+	}
+	printf("\n");
+
+	return 0;
 }
